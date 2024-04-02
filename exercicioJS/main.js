a838722434f859ec7ee72fc4e5ea6e1c7cc4fcc9
Daniel Cardoso <danielmcardoso2016@protonmail.com>
2024-04-02 16:06:46 -0400

    atualizando

--- exercicioJS/main.js
+++ exercicioJS/main.js
@@ -1,6 +1,7 @@
 const form = document.getElementById('form')
 const valorA = document.getElementById('a')
 const valorB = document.getElementById('b')
+const body = document.getElementById('body')
 
 const mensagemSucesso = document.querySelector('.sucesso')
 const mensagemNeutro = document.querySelector('.neutro')
@@ -13,29 +14,37 @@ form.addEventListener('submit', function(e){
     const error = 'Erro!'
     const neutro = 'Valores Iguais!'
 
+    const red = '#c84242'
+    const green = '#5fc842'
+    const yellow = '#a6c842'
+
     if (valorA.value < valorB.value){
-        
+        body.style.backgroundColor = green
         mensagemSucesso.innerHTML = sucesso
-        mensagemSucesso.style.display = 'block'
+        mensagemSucesso.style.display = 'inline-block'
+        
         mensagemErro.style = ''
         mensagemNeutro.style = ''
 
         valorA.value = ''
         valorB.value = ''
     }else if (valorA.value == valorB.value){
-        
+        body.style.backgroundColor = yellow
         mensagemNeutro.innerHTML = neutro
-        mensagemNeutro.style.display = 'block'
+        mensagemNeutro.style.display = 'inline-block'
+        
         mensagemErro.style = ''
         mensagemSucesso.style = ''
 
         valorA.value = ''
         valorB.value = ''
     }else{
-        
+        body.style.backgroundColor = red
         mensagemErro.innerHTML = error
-        mensagemErro.style.display = 'block'
+        mensagemErro.style.display = 'inline-block'
+        
         mensagemNeutro.style = ''
+        mensagemSucesso.style = ''
     }
 })
 
