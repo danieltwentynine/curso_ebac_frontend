05bde9951b10360e5f32a4ab01bfb466acff3e3b
Daniel M Cardoso <88412799+danieltwentynine@users.noreply.github.com>
2024-08-01 21:23:46 -0300

    exercicio-ajax

--- main.js
+++ main.js
@@ -1,66 +1,28 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   document.getElementById("btn-buscar-cep").addEventListener("click", function () {
-//     const xhttp = new XMLHttpRequest();
-//     const cep = document.getElementById('cep').value;
-//     const endpoint = `https://viacep.com.br/ws/${cep}/json`;
-
-//     xhttp.open('GET', endpoint);
-//     xhttp.send();
-//   });
-// });
-
 $(document).ready(function(){
-  $('#cep').mask('00000-000');
+    const nameElement = $('#name');
+    const usernameElement = $('#username');
+    const avatarElement = $('#avatar');
+    const reposElement = $('#repos');
+    const followersElement = $('#followers');
+    const followingElement = $('#following');
+    const linkElement = $('#link');
 
-  $('#btn-buscar-cep').click(function(){
-    const cep = $('#cep').val();
-    const endpoint = `https://viacep.com.br/ws/${cep}/json`;
-
-    const btn = $(this);
-
-    $(btn).find('i').addClass('d-none');
-    $(btn).find('span').removeClass('d-none');
-
-    // AJAX com jQuery
-    // $.ajax(endpoint).done(function(resposta){
-    //   const logradouro = resposta.logradouro;
-    //   const bairro = resposta.bairro;
-    //   const cidade = resposta.localidade;
-    //   const estado = resposta.uf;
-    //   const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
-
-    //   $('#endereco').val(endereco);
-
-    //   $(btn).find('i').removeClass('d-none');
-    //   $(btn).find('span').addClass('d-none');
-    // })
-
-    // fetch API
-    fetch(endpoint).then(function(resposta){
-      return resposta.json();
+    const endpoint = 'https://api.github.com/users/danieltwentynine';
+    
+    fetch(endpoint).then(function(res){
+        return res.json();
     })
     .then(function(json){
-      const logradouro = json.logradouro;
-      const bairro = json.bairro;
-      const cidade = json.localidade;
-      const estado = json.uf;
-      const endereco = `${logradouro}, ${bairro} - ${cidade} - ${estado}`;
-      $('#endereco').val(endereco);
+        nameElement.text(json.name);
+        usernameElement.text(json.login);
+        avatarElement.attr('src', json.avatar_url);
+        avatarElement.attr('alt', json.name);
+        reposElement.text(json.public_repos);
+        followersElement.text(json.followers);
+        followingElement.text(json.following);
+        linkElement.attr('href', json.html_url);
     })
     .catch(function(erro){
-      alert("Ocorreu um erro ao buscar o endereço, tente novamente!");
+        alert('Ocorreu um erro ao buscar dados da API.');
     })
-    .finally(function(){
-      $(btn).find('i').removeClass('d-none');
-      $(btn).find('span').addClass('d-none');
-    })
-  })
-
-  $('#form-pedido').submit(function(event) {
-    event.preventDefault();
-
-    if($('#nome').val().length == 0) {
-      throw new Error('Digite seu nome');
-    }
-  })
 })
